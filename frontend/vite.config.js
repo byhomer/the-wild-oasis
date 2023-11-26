037c90a357516c03c102c28b014561ac7030d56c
Amer Delic <info@delic-it.ch>
2023-11-26 19:55:50 +0100

    wrong vite create

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-import eslint from 'vite-plugin-eslint'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), eslint()],
+  plugins: [react(), eslint()],
 })
